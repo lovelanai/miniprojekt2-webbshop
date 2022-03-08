7cfaab4a5757b7805d8b26d717336d6542d40fcb
William Saar <williamsaar@outlook.com>
2022-03-08 16:25:23 +0100

    added shoppingcart context
    Co-authored-by: Erik Matsegård <matsegard@outlook.com>

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,13 +2,18 @@ import { Button } from "@mui/material";
 import ProductProvider from "../contexts/ProductContext";
 import "./App.css";
 import Layout from "./Layout";
+import ShoppingCartProvider from "../contexts/ProductContext";
 
 function App() {
   return (
     // <BrowserRouter>
+
     <ProductProvider>
-      <Layout />
+      <ShoppingCartProvider>
+        <Layout />
+      </ShoppingCartProvider>
     </ProductProvider>
+
     // </BrowserRouter>
   );
 }
