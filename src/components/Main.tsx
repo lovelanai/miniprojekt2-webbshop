5f6429e8c75be95c580272c85f82158135ae82c2
Max Andersson <maxbjornkga@gmail.com>
2022-03-09 11:54:32 +0100

    Link to shoppingCartPage Co-authored-by: lovelanai <lovelanai@users.noreply.github.com>

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,10 +1,40 @@
+
+import { Routes, Route } from "react-router-dom";
+import ShoppingCartPage from "./ShoppingCartPage";
 import Store from "./Store";
 
 function Main() {
   return (
     <main>
-      <Store />
-    </main>
+
+      <Routes>
+        <Route
+          path="/"
+          element={<Store />}
+        />
+        <Route
+          path="/ShoppingCartPage"
+          element={
+            <ShoppingCartPage />}
+        />
+
+      </Routes>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </main >
   );
 }
 
