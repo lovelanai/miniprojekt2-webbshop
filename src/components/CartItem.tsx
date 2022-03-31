c49362c70be44c556586c0caeeff9f7ea936a765
William Saar <williamsaar@outlook.com>
2022-03-31 09:52:23 +0200

    fixed button colors in checkout

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -4,11 +4,29 @@ import {
   CardActions,
   CardContent,
   CardMedia,
+  createTheme,
+  ThemeProvider,
   Typography,
 } from "@mui/material";
 import { useContext } from "react";
 import { ShoppingCartContext, useCart } from "../contexts/ShoppingCartContext";
 import "./ShoppingCart.css";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#333333",
+      contrastText: "#FBF7F5", //button text white instead of black
+    },
+    background: {
+      default: "#333333",
+    },
+
+    secondary: {
+      main: "#DA344D",
+    },
+  },
+});
 
 export default function CartItem(): JSX.Element {
   const { cartItems } = useContext(ShoppingCartContext);
@@ -25,52 +43,54 @@ export default function CartItem(): JSX.Element {
   }
 
   return (
-    <div>
-      <div className="test-container">
-        {cartItems.map((item) => (
-          <div key={item.id}>
-            <Card className="test">
-              <div className="image">
-                <h1>{item.title}</h1>
-                <CardMedia
-                  component="img"
-                  alt={item.title}
-                  height="auto"
-                  image={item.image}
-                  title={item.title}
-                />
-              </div>
+    <ThemeProvider theme={theme}>
+      <div>
+        <div className="test-container">
+          {cartItems.map((item) => (
+            <div key={item.id}>
+              <Card className="test">
+                <div className="image">
+                  <h1>{item.title}</h1>
+                  <CardMedia
+                    component="img"
+                    alt={item.title}
+                    height="auto"
+                    image={item.image}
+                    title={item.title}
+                  />
+                </div>
 
-              <div className="product-info">
-                <CardContent>
-                  <Typography variant="body2" color="text.secondary">
-                    {item.price}:-
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button
-                    variant="contained"
-                    size="small"
-                    onClick={() => handleRemoveProduct(item)}
-                  >
-                    -
-                  </Button>
+                <div className="product-info">
+                  <CardContent>
+                    <Typography variant="body2" color="text.secondary">
+                      {item.price}:-
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button
+                      variant="contained"
+                      size="small"
+                      onClick={() => handleRemoveProduct(item)}
+                    >
+                      -
+                    </Button>
 
-                  <Typography>{item.quantity} </Typography>
-                  <Button
-                    variant="contained"
-                    size="small"
-                    onClick={() => handleAddProduct(item)}
-                  >
-                    +
-                  </Button>
-                </CardActions>
-                <Typography>{item.quantity * item.price}:- </Typography>
-              </div>
-            </Card>
-          </div>
-        ))}
+                    <Typography>{item.quantity} </Typography>
+                    <Button
+                      variant="contained"
+                      size="small"
+                      onClick={() => handleAddProduct(item)}
+                    >
+                      +
+                    </Button>
+                  </CardActions>
+                  <Typography>{item.quantity * item.price}:- </Typography>
+                </div>
+              </Card>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
